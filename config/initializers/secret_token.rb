640acab2940bf45f7fadbc9bfb2a38c55a7a28ce
lightyrs <harris@harrisnovick.com>
2011-05-26 23:55:59 -0400

    Starting over with a working version of Rake.

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Grokpanel::Application.config.secret_token = '6a9ec54068218d74dd4ddebf86c2927c4cc1149b0878c441f99fe40b49eb505efa400c3d75e106d10a96bded4054526f976db26c0d06d1b3e226bb0e63196274'
